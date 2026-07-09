87c42ba928141dce91d15744c71b4b10eb549427
Yashi <yashigupta052422@gmail.com>
2026-07-09 23:50:59 +0530

    Day9:SORTING

--- sorting/(75)sortcolors.py
+++ sorting/(75)sortcolors.py
@@ -36,6 +36,26 @@ class Solution:
             
             
 #approach 3:Dutch National Flag Algorithm (Best)
+class Solution(object):
+    def sortColors(self, nums):
+
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
             
         
     
